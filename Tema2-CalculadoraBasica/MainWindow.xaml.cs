8e3d6a7da1d301f611526be41d9451cc45fb0bb7
manue <manue@P02HP2023>
2023-09-30 12:10:25 +0200

    Agregar archivos de proyecto.

---
+++ Tema2-CalculadoraBasica/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Tema2_CalculadoraBasica
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calcularButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(operando1TextBox.Text) || String.IsNullOrEmpty(operando2TextBox.Text))
+                MessageBox.Show("Se ha producido un error. Revise los operandos.");
+            else
+            {
+                try
+                {
+                    int n1 = int.Parse(operando1TextBox.Text);
+                    int n2 = int.Parse(operando2TextBox.Text);
+                    switch (operadorTextBox.Text)
+                    {
+                        case "+":
+                            resultadoTextBox.Text = (n1 + n2).ToString();
+                            break;
+                        case "-":
+                            resultadoTextBox.Text = (n1 - n2).ToString();
+                            break;
+                        case "*":
+                            resultadoTextBox.Text = (n1 * n2).ToString();
+                            break;
+                        case "/":
+                            resultadoTextBox.Text = (n1 / n2).ToString();
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Se ha producido un error. Revise los operandos.");
+                }
+            }
+        }
+
+        private void operadorTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string op = operadorTextBox.Text;
+            if (op == "+" || op == "-" || op == "*"|| op == "/")
+            {
+                calcularButton.IsEnabled = true;
+            }
+            else calcularButton.IsEnabled = false;
+        }
+
+        private void limpiarButton_Click(object sender, RoutedEventArgs e)
+        {
+            operando1TextBox.Clear();
+            operando2TextBox.Clear();
+            operadorTextBox.Clear();
+            resultadoTextBox.Clear();
+        }
+    }
+}
